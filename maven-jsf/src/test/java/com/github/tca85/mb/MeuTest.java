ce2181893dd9f7be03ff1cffc8dcf058ff5770d6
tca85 <thiago.alves.85@gmail.com>
2015-12-03 22:30:03 -0500

    testes com maven

---
+++ maven-jsf/src/test/java/com/github/tca85/mb/MeuTest.java
@@ -0,0 +1,12 @@
+package com.github.tca85.mb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MeuTest {
+	@Test
+	public void primeiroTeste() throws Exception {
+		assertTrue(true);
+	}
+}
